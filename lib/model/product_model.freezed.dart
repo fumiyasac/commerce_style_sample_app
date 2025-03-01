0031235a356879cb4b60996c4e6a3275a62e9244
fumiyasac <just1factory@gmail.com>
2025-03-01 15:31:49 +0900

    ProductModelの新規作成

---
+++ lib/model/product_model.freezed.dart
@@ -0,0 +1,473 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'product_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+ProductModel _$ProductModelFromJson(Map<String, dynamic> json) {
+  return _ProductModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$ProductModel {
+  int get id => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  String get description => throw _privateConstructorUsedError;
+  int get price => throw _privateConstructorUsedError;
+  double get discountPercentage => throw _privateConstructorUsedError;
+  double get rating => throw _privateConstructorUsedError;
+  int get stock => throw _privateConstructorUsedError;
+  String get brand => throw _privateConstructorUsedError;
+  String get category => throw _privateConstructorUsedError;
+  String get warrantyInformation => throw _privateConstructorUsedError;
+  String get shippingInformation => throw _privateConstructorUsedError;
+  String get availabilityStatus => throw _privateConstructorUsedError;
+  String get returnPolicy => throw _privateConstructorUsedError;
+  String get minimumOrderQuantity => throw _privateConstructorUsedError;
+  String get thumbnail => throw _privateConstructorUsedError;
+
+  /// Serializes this ProductModel to a JSON map.
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+
+  /// Create a copy of ProductModel
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $ProductModelCopyWith<ProductModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ProductModelCopyWith<$Res> {
+  factory $ProductModelCopyWith(
+          ProductModel value, $Res Function(ProductModel) then) =
+      _$ProductModelCopyWithImpl<$Res, ProductModel>;
+  @useResult
+  $Res call(
+      {int id,
+      String title,
+      String description,
+      int price,
+      double discountPercentage,
+      double rating,
+      int stock,
+      String brand,
+      String category,
+      String warrantyInformation,
+      String shippingInformation,
+      String availabilityStatus,
+      String returnPolicy,
+      String minimumOrderQuantity,
+      String thumbnail});
+}
+
+/// @nodoc
+class _$ProductModelCopyWithImpl<$Res, $Val extends ProductModel>
+    implements $ProductModelCopyWith<$Res> {
+  _$ProductModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of ProductModel
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? title = null,
+    Object? description = null,
+    Object? price = null,
+    Object? discountPercentage = null,
+    Object? rating = null,
+    Object? stock = null,
+    Object? brand = null,
+    Object? category = null,
+    Object? warrantyInformation = null,
+    Object? shippingInformation = null,
+    Object? availabilityStatus = null,
+    Object? returnPolicy = null,
+    Object? minimumOrderQuantity = null,
+    Object? thumbnail = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      price: null == price
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as int,
+      discountPercentage: null == discountPercentage
+          ? _value.discountPercentage
+          : discountPercentage // ignore: cast_nullable_to_non_nullable
+              as double,
+      rating: null == rating
+          ? _value.rating
+          : rating // ignore: cast_nullable_to_non_nullable
+              as double,
+      stock: null == stock
+          ? _value.stock
+          : stock // ignore: cast_nullable_to_non_nullable
+              as int,
+      brand: null == brand
+          ? _value.brand
+          : brand // ignore: cast_nullable_to_non_nullable
+              as String,
+      category: null == category
+          ? _value.category
+          : category // ignore: cast_nullable_to_non_nullable
+              as String,
+      warrantyInformation: null == warrantyInformation
+          ? _value.warrantyInformation
+          : warrantyInformation // ignore: cast_nullable_to_non_nullable
+              as String,
+      shippingInformation: null == shippingInformation
+          ? _value.shippingInformation
+          : shippingInformation // ignore: cast_nullable_to_non_nullable
+              as String,
+      availabilityStatus: null == availabilityStatus
+          ? _value.availabilityStatus
+          : availabilityStatus // ignore: cast_nullable_to_non_nullable
+              as String,
+      returnPolicy: null == returnPolicy
+          ? _value.returnPolicy
+          : returnPolicy // ignore: cast_nullable_to_non_nullable
+              as String,
+      minimumOrderQuantity: null == minimumOrderQuantity
+          ? _value.minimumOrderQuantity
+          : minimumOrderQuantity // ignore: cast_nullable_to_non_nullable
+              as String,
+      thumbnail: null == thumbnail
+          ? _value.thumbnail
+          : thumbnail // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ProductModelImplCopyWith<$Res>
+    implements $ProductModelCopyWith<$Res> {
+  factory _$$ProductModelImplCopyWith(
+          _$ProductModelImpl value, $Res Function(_$ProductModelImpl) then) =
+      __$$ProductModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {int id,
+      String title,
+      String description,
+      int price,
+      double discountPercentage,
+      double rating,
+      int stock,
+      String brand,
+      String category,
+      String warrantyInformation,
+      String shippingInformation,
+      String availabilityStatus,
+      String returnPolicy,
+      String minimumOrderQuantity,
+      String thumbnail});
+}
+
+/// @nodoc
+class __$$ProductModelImplCopyWithImpl<$Res>
+    extends _$ProductModelCopyWithImpl<$Res, _$ProductModelImpl>
+    implements _$$ProductModelImplCopyWith<$Res> {
+  __$$ProductModelImplCopyWithImpl(
+      _$ProductModelImpl _value, $Res Function(_$ProductModelImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of ProductModel
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? title = null,
+    Object? description = null,
+    Object? price = null,
+    Object? discountPercentage = null,
+    Object? rating = null,
+    Object? stock = null,
+    Object? brand = null,
+    Object? category = null,
+    Object? warrantyInformation = null,
+    Object? shippingInformation = null,
+    Object? availabilityStatus = null,
+    Object? returnPolicy = null,
+    Object? minimumOrderQuantity = null,
+    Object? thumbnail = null,
+  }) {
+    return _then(_$ProductModelImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      price: null == price
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as int,
+      discountPercentage: null == discountPercentage
+          ? _value.discountPercentage
+          : discountPercentage // ignore: cast_nullable_to_non_nullable
+              as double,
+      rating: null == rating
+          ? _value.rating
+          : rating // ignore: cast_nullable_to_non_nullable
+              as double,
+      stock: null == stock
+          ? _value.stock
+          : stock // ignore: cast_nullable_to_non_nullable
+              as int,
+      brand: null == brand
+          ? _value.brand
+          : brand // ignore: cast_nullable_to_non_nullable
+              as String,
+      category: null == category
+          ? _value.category
+          : category // ignore: cast_nullable_to_non_nullable
+              as String,
+      warrantyInformation: null == warrantyInformation
+          ? _value.warrantyInformation
+          : warrantyInformation // ignore: cast_nullable_to_non_nullable
+              as String,
+      shippingInformation: null == shippingInformation
+          ? _value.shippingInformation
+          : shippingInformation // ignore: cast_nullable_to_non_nullable
+              as String,
+      availabilityStatus: null == availabilityStatus
+          ? _value.availabilityStatus
+          : availabilityStatus // ignore: cast_nullable_to_non_nullable
+              as String,
+      returnPolicy: null == returnPolicy
+          ? _value.returnPolicy
+          : returnPolicy // ignore: cast_nullable_to_non_nullable
+              as String,
+      minimumOrderQuantity: null == minimumOrderQuantity
+          ? _value.minimumOrderQuantity
+          : minimumOrderQuantity // ignore: cast_nullable_to_non_nullable
+              as String,
+      thumbnail: null == thumbnail
+          ? _value.thumbnail
+          : thumbnail // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$ProductModelImpl implements _ProductModel {
+  const _$ProductModelImpl(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.price,
+      required this.discountPercentage,
+      required this.rating,
+      required this.stock,
+      required this.brand,
+      required this.category,
+      required this.warrantyInformation,
+      required this.shippingInformation,
+      required this.availabilityStatus,
+      required this.returnPolicy,
+      required this.minimumOrderQuantity,
+      required this.thumbnail});
+
+  factory _$ProductModelImpl.fromJson(Map<String, dynamic> json) =>
+      _$$ProductModelImplFromJson(json);
+
+  @override
+  final int id;
+  @override
+  final String title;
+  @override
+  final String description;
+  @override
+  final int price;
+  @override
+  final double discountPercentage;
+  @override
+  final double rating;
+  @override
+  final int stock;
+  @override
+  final String brand;
+  @override
+  final String category;
+  @override
+  final String warrantyInformation;
+  @override
+  final String shippingInformation;
+  @override
+  final String availabilityStatus;
+  @override
+  final String returnPolicy;
+  @override
+  final String minimumOrderQuantity;
+  @override
+  final String thumbnail;
+
+  @override
+  String toString() {
+    return 'ProductModel(id: $id, title: $title, description: $description, price: $price, discountPercentage: $discountPercentage, rating: $rating, stock: $stock, brand: $brand, category: $category, warrantyInformation: $warrantyInformation, shippingInformation: $shippingInformation, availabilityStatus: $availabilityStatus, returnPolicy: $returnPolicy, minimumOrderQuantity: $minimumOrderQuantity, thumbnail: $thumbnail)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ProductModelImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.description, description) ||
+                other.description == description) &&
+            (identical(other.price, price) || other.price == price) &&
+            (identical(other.discountPercentage, discountPercentage) ||
+                other.discountPercentage == discountPercentage) &&
+            (identical(other.rating, rating) || other.rating == rating) &&
+            (identical(other.stock, stock) || other.stock == stock) &&
+            (identical(other.brand, brand) || other.brand == brand) &&
+            (identical(other.category, category) ||
+                other.category == category) &&
+            (identical(other.warrantyInformation, warrantyInformation) ||
+                other.warrantyInformation == warrantyInformation) &&
+            (identical(other.shippingInformation, shippingInformation) ||
+                other.shippingInformation == shippingInformation) &&
+            (identical(other.availabilityStatus, availabilityStatus) ||
+                other.availabilityStatus == availabilityStatus) &&
+            (identical(other.returnPolicy, returnPolicy) ||
+                other.returnPolicy == returnPolicy) &&
+            (identical(other.minimumOrderQuantity, minimumOrderQuantity) ||
+                other.minimumOrderQuantity == minimumOrderQuantity) &&
+            (identical(other.thumbnail, thumbnail) ||
+                other.thumbnail == thumbnail));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      id,
+      title,
+      description,
+      price,
+      discountPercentage,
+      rating,
+      stock,
+      brand,
+      category,
+      warrantyInformation,
+      shippingInformation,
+      availabilityStatus,
+      returnPolicy,
+      minimumOrderQuantity,
+      thumbnail);
+
+  /// Create a copy of ProductModel
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ProductModelImplCopyWith<_$ProductModelImpl> get copyWith =>
+      __$$ProductModelImplCopyWithImpl<_$ProductModelImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$ProductModelImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _ProductModel implements ProductModel {
+  const factory _ProductModel(
+      {required final int id,
+      required final String title,
+      required final String description,
+      required final int price,
+      required final double discountPercentage,
+      required final double rating,
+      required final int stock,
+      required final String brand,
+      required final String category,
+      required final String warrantyInformation,
+      required final String shippingInformation,
+      required final String availabilityStatus,
+      required final String returnPolicy,
+      required final String minimumOrderQuantity,
+      required final String thumbnail}) = _$ProductModelImpl;
+
+  factory _ProductModel.fromJson(Map<String, dynamic> json) =
+      _$ProductModelImpl.fromJson;
+
+  @override
+  int get id;
+  @override
+  String get title;
+  @override
+  String get description;
+  @override
+  int get price;
+  @override
+  double get discountPercentage;
+  @override
+  double get rating;
+  @override
+  int get stock;
+  @override
+  String get brand;
+  @override
+  String get category;
+  @override
+  String get warrantyInformation;
+  @override
+  String get shippingInformation;
+  @override
+  String get availabilityStatus;
+  @override
+  String get returnPolicy;
+  @override
+  String get minimumOrderQuantity;
+  @override
+  String get thumbnail;
+
+  /// Create a copy of ProductModel
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$ProductModelImplCopyWith<_$ProductModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
