c0bbdd95db242009a75188bdc9313b66e91e554d
fumiyasac <just1factory@gmail.com>
2025-03-02 19:17:24 +0900

    ProductResponseモデルの新規作成

---
+++ lib/models/product_response.dart
@@ -0,0 +1,26 @@
+import 'package:commerce_style_sample_app/models/product.dart';
+
+class ProductResponse {
+  final List<Product> products;
+  final int total;
+  final int skip;
+  final int limit;
+
+  ProductResponse({
+    required this.products,
+    required this.total,
+    required this.skip,
+    required this.limit,
+  });
+
+  factory ProductResponse.fromJson(Map<String, dynamic> json) {
+    return ProductResponse(
+      products: (json['products'] as List)
+          .map((item) => Product.fromJson(item))
+          .toList(),
+      total: json['total'],
+      skip: json['skip'],
+      limit: json['limit'],
+    );
+  }
+}
