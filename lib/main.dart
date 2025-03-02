43614a838a297b8e60a103521e52772950e2eb3f
fumiyasac <just1factory@gmail.com>
2025-03-02 17:24:57 +0900

    Riverpodを利用可能にする

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:commerce_style_sample_app/view/item_list_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
