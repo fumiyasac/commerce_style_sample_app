d0838fbfc1ec403bc612d9fe86fd143f85c9c91f
fumiyasac <just1factory@gmail.com>
2025-03-02 13:22:42 +0900

    Controllerクラスの新規作成

---
+++ lib/controller/product_controller.dart
@@ -0,0 +1,29 @@
+import 'package:commerce_style_sample_app/repository/product_repository.dart';
+import 'package:commerce_style_sample_app/state/product_list_state.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'product_controller.g.dart';
+
+@riverpod
+class ProductController extends _$ProductController {
+  @override
+  ProductListState build() => ProductListState(
+    productList: [],
+    loading: false,
+    maxLoaded: false,
+  );
+
+  void loadProductList () async {
+    if (state.loading || state.maxLoaded) {
+      return;
+    }
+    state = state.copyWith(loading: true);
+    final repository = ref.read(productRepositoryProvider);
+    final productList = await repository.getProducts(offset: state.productList.length);
+    if (productList.isEmpty) {
+      state = state.copyWith(maxLoaded: true, loading: false);
+    } else {
+      state = state.copyWith(productList: [...state.productList, ...productList], loading: false);
+    }
+  }
+}
---
+++ lib/controller/product_controller.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$productControllerHash() => r'b1eb3986ca739d082c7af7af8912d42311efb9fc';
+
+/// See also [ProductController].
+@ProviderFor(ProductController)
+final productControllerProvider =
+    AutoDisposeNotifierProvider<ProductController, ProductListState>.internal(
+  ProductController.new,
+  name: r'productControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$productControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ProductController = AutoDisposeNotifier<ProductListState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
